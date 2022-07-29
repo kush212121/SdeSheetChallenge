29ad348cbccecfdf1ef2b432323f63418a8eeae6
Kushagra Bajpai <kushagra.25bajpai@gmail.com>
2022-07-29 23:09:47 +0530

    Recursion and Backtracking
    Recursion and Backtracking

---
+++ Recursion and Backtracking/subsetsum.java
@@ -0,0 +1,24 @@
+package Recursion and Backtracking;
+
+public class subsetsum {
+    static void func(int ind, int sum, ArrayList < Integer > arr, int N, ArrayList < Integer > sumSubset) {
+        if (ind == N) {
+            sumSubset.add(sum);
+            return;
+        }
+
+        // pick the element 
+        func(ind + 1, sum + arr.get(ind), arr, N, sumSubset);
+
+        // Do-not pick the element
+        func(ind + 1, sum, arr, N, sumSubset);
+    }
+
+    static ArrayList < Integer > subsetSums(ArrayList < Integer > arr, int N) {
+
+        ArrayList < Integer > sumSubset = new ArrayList < > ();
+        func(0, 0, arr, N, sumSubset);
+        Collections.sort(sumSubset);
+        return sumSubset;
+    }
+}
